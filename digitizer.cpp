ccb15784f77ac0e7bd0320a40b2a9522044a75fa
lbruni <lbruni@nikhef.nl>
2015-09-30 18:37:42 +0200

    added digitizer class

---
+++ digitizer.cpp
@@ -0,0 +1,45 @@
+
+
+#include "digitizer.h"
+#define nChannels_Analog 128
+#define nChannels_Binary 2
+#define Charge_min 0
+#define Charge_max 1
+
+Double_t digitizer::preAmplifier(Double_t chargefC){
+    Double_t chargemV;
+    chargemV = 95.0 * chargefC;
+    std::cout<< " charge [fC]:  "<<chargefC<<" charge [mV]:  "<<chargemV<<std::endl;
+    return chargemV;
+}
+
+void digitizer::AlibavaReadout( std::vector<hit_with_charge> m_strip_position){
+   
+    canvas = new TCanvas("canvas","canvas",500,500);
+    analog = new TH2D("analog","analog", 100 ,0, 7.32600 , 128 ,0, 1);
+    
+    for(int i=0; i <m_strip_position.size(); i++){
+        
+        if (m_strip_position[i].charge > 0.00005 && m_strip_position[i].charge< 1 ) {
+            
+            analog->Fill(m_strip_position[i].x,m_strip_position[i].charge);
+        }
+    }
+    TH1D *hist0_ProjX = analog->ProjectionY();
+    analog->Draw("colz");
+}
+
+void digitizer::BinaryReadout(std::vector<hit_with_charge> m_strip_position){
+    
+    canvas1 = new TCanvas("canvas1","canvas1",500,500);
+    binary = new TH2D("binary","binary",100,0,7.32600, 2,0,1);
+    
+    for(int i=0; i <m_strip_position.size(); i++){
+        
+        if (m_strip_position[i].charge > 0.00005 && m_strip_position[i].charge< 1 ) {
+            
+            binary->Fill(m_strip_position[i].x,m_strip_position[i].charge);
+        }
+        binary->Draw("colz ");
+    }
+}
---
+++ digitizer.h
@@ -0,0 +1,30 @@
+//
+//  digitizer.h
+//  
+//
+//  Created by Lucrezia Stella Bruni on 28/09/15.
+//
+//
+
+#ifndef ____digitizer__
+#define ____digitizer__
+
+#include <stdio.h>
+class digitizer {
+public:
+    TCanvas *canvas;
+    TCanvas *canvas1;
+    Double_t matrix_analog[100][128];
+    Double_t matrix_binary[100][2];
+    TH2D *binary = NULL;
+    TH2D *analog = NULL;
+    Double_t preAmplifier(Double_t chargefC);
+    Double_t chargemV;
+    void BinaryReadout(std::vector<hit_with_charge> m_strip_position);
+    void AlibavaReadout( std::vector<hit_with_charge> m_strip_position);
+    
+    Int_t hit_binary;
+    
+};
+
+#endif /* defined(____digitizer__) */
