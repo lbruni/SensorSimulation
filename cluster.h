6facc99939e9bb69a6aa18a1379d22f04780bc20
rpeschke <richard.peschke@desy.de>
2015-10-12 11:52:15 +0200

    added new member function to store the cluster data
    moved the find cluster code to the process event function to make the code more uniform

--- cluster.h
+++ cluster.h
@@ -3,19 +3,22 @@
 #define ____cluster__
 
 #include <stdio.h>
-
+#include "cluster_struct.h"
 class cluster {
 public:
     
-    Int_t cont;
     void  set_intput_pointer_digitizer(const std::vector<hit_with_charge>* input_pointer);
-    void FindClusterNumber();
+
     void processEvent();
     
+    const cluster_struct* get_cluster_ptr() const;
+    size_t find_first_cluster(size_t start);
+    double get_cluster_size() const;
 private:
+  
+
     const std::vector<hit_with_charge>*  m_digitized_hit;
-    const std::vector<hit_with_charge>* get_hit_ptr() const;
-    
+    cluster_struct m_cluster;
 #endif /* defined(____cluster__) */
     
     
